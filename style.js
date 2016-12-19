f06bae425dd2d5b60d05bc85c622a5aeb37e7827
Christophe Bonin <christophe.bonin@gmail.com>
2016-12-19 13:03:27 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Christophe Bonin on 18/12/2016.
+ */
+function myMove (){
+    document.getElementById('figure').style.marginLeft= "43.5%";
+    document.getElementById('figure').style.animation= "bounce 0.3s ease 15 alternate";
+    document.getElementById('figure').style.WebkitAnimation= "bounce 0.3s ease 15 alternate"; /* For safari 4.0 - 8.0 */
+}
